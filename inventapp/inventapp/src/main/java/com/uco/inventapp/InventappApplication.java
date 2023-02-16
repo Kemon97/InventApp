7e30686b269aaf9d96638d38d167c19d891f6968
Kemon97 <kevin.montoya5146@uco.net.co>
2023-02-15 21:19:40 -0500

    Spring Start
    API

---
+++ inventapp/inventapp/src/main/java/com/uco/inventapp/InventappApplication.java
@@ -0,0 +1,13 @@
+package com.uco.inventapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventappApplication.class, args);
+	}
+
+}
